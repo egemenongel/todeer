45f145c1f0797c79f75cb6570e148672cc9a660c
Egemen <egemenongel@hotmail.com>
2021-11-15 20:41:22 +0300

    Add sign in and sign out methods

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,4 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth;
+  AuthService(this._firebaseAuth);
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+  Future<String?> signIn({String? email, String? password}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email!, password: password!);
+      print(_firebaseAuth.currentUser!.uid);
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      return e.code;
+    }
+  }
+
+  void signOut() {
+    _firebaseAuth.signOut();
+  }
+}
