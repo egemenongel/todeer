d350a4471206e0a72689b7f814e99821040a561d
Egemen <egemenongel@hotmail.com>
2021-11-16 09:21:12 +0300

    Add toggle to switch between login & sign up

--- lib/utils/auth_wrapper.dart
+++ lib/utils/auth_wrapper.dart
@@ -2,10 +2,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:to_deer/pages/login_page.dart';
+import 'package:to_deer/pages/sign_up_page.dart';
 import 'package:to_deer/pages/task_lists_page.dart';
 
-class AuthWrapper extends StatelessWidget {
+class AuthWrapper extends StatefulWidget {
   const AuthWrapper({Key? key}) : super(key: key);
+
+  @override
+  State<AuthWrapper> createState() => _AuthWrapperState();
+}
+
+class _AuthWrapperState extends State<AuthWrapper> {
+  bool showLogin = true;
+  void toggle() {
+    setState(() {
+      showLogin = !showLogin;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +26,10 @@ class AuthWrapper extends StatelessWidget {
     if (user != null) {
       return const TaskListsPage();
     }
-    return LoginPage();
+    if (showLogin) {
+      return LoginPage(toggleCallback: toggle);
+    } else {
+      return SignUpPage(toggleCallback: toggle);
+    }
   }
 }
