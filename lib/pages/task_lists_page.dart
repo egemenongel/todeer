862d3ad69461c51ae1451aa299b965e169403846
Egemen <egemenongel@hotmail.com>
2021-11-18 12:06:06 +0300

    Update circular progress indicator

--- lib/pages/task_lists_page.dart
+++ lib/pages/task_lists_page.dart
@@ -13,92 +13,95 @@ class TaskListsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var firestore = DatabaseService();
-    return StreamBuilder(
-      stream: firestore.listsCollection.snapshots(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return Scaffold(
-          key: _key,
-          extendBodyBehindAppBar: true,
-          endDrawer: SizedBox(
-            width: displayWidth(context) / 3,
-            child: Drawer(
-                child: Container(
-              color: Colors.indigoAccent,
-              child: ListView(
-                children: [
-                  const ListTile(
-                    title: CircleAvatar(
-                      child: Icon(
-                        Icons.person,
-                      ),
-                    ),
+    return Scaffold(
+      key: _key,
+      extendBodyBehindAppBar: true,
+      endDrawer: SizedBox(
+        width: displayWidth(context) / 3,
+        child: Drawer(
+            child: Container(
+          color: Colors.indigoAccent,
+          child: ListView(
+            children: [
+              const ListTile(
+                title: CircleAvatar(
+                  child: Icon(
+                    Icons.person,
                   ),
-                  ListTile(
-                    title: Text(
-                      "${FirebaseAuth.instance.currentUser!.email}",
-                      style: const TextStyle(
-                        fontSize: 10.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                    title: IconButton(
-                        onPressed: () => context.read<AuthService>().signOut(),
-                        icon: const Icon(
-                          Icons.power_settings_new,
-                          color: Colors.white,
-                        )),
-                  )
-                ],
+                ),
               ),
-            )),
-          ),
-          appBar: AppBar(
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    _key.currentState!.openEndDrawer();
-                  },
-                  icon: const Icon(
-                    Icons.menu_rounded,
+              ListTile(
+                title: Text(
+                  "${FirebaseAuth.instance.currentUser!.email}",
+                  style: const TextStyle(
+                    fontSize: 10.0,
                     color: Colors.white,
-                  )),
-            ],
-            toolbarHeight: appBarHeight(context),
-            backgroundColor: Colors.red,
-            shadowColor: Colors.indigo,
-            title: SizedBox(
-              height: logoHeight(context),
-              child: const Image(
-                fit: BoxFit.fitWidth,
-                color: Colors.white,
-                image: AssetImage("images/icons/24.png"),
+                  ),
+                ),
               ),
-            ),
-            shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(50),
-            )),
+              ListTile(
+                title: IconButton(
+                    onPressed: () => context.read<AuthService>().signOut(),
+                    icon: const Icon(
+                      Icons.power_settings_new,
+                      color: Colors.white,
+                    )),
+              )
+            ],
           ),
-          body: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(bottomRight: Radius.circular(50)),
-              color: Colors.blueAccent,
-            ),
-            child: Column(
-              children: [
-                Expanded(
-                    child: Container(
-                        padding: const EdgeInsets.all(20),
-                        child: Column(
-                          children: [
-                            Expanded(
+        )),
+      ),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                _key.currentState!.openEndDrawer();
+              },
+              icon: const Icon(
+                Icons.menu_rounded,
+                color: Colors.white,
+              )),
+        ],
+        toolbarHeight: appBarHeight(context),
+        backgroundColor: Colors.red,
+        shadowColor: Colors.indigo,
+        title: SizedBox(
+          height: logoHeight(context),
+          child: const Image(
+            fit: BoxFit.fitWidth,
+            color: Colors.white,
+            image: AssetImage("images/icons/24.png"),
+          ),
+        ),
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(50),
+        )),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(bottomRight: Radius.circular(50)),
+          color: Colors.blueAccent,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+                child: Container(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        StreamBuilder(
+                          stream: firestore.listsCollection.snapshots(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (!snapshot.hasData) {
+                              return const Center(
+                                child: CircularProgressIndicator(
+                                  color: Colors.red,
+                                ),
+                              );
+                            }
+                            return Expanded(
                               child: ListView.separated(
                                 itemCount: snapshot.data.docs.length,
                                 itemBuilder: (BuildContext context, int index) {
@@ -114,24 +117,24 @@ class TaskListsPage extends StatelessWidget {
                                       height: 20, color: Colors.transparent);
                                 },
                               ),
-                            ),
-                          ],
-                        ))),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.deepOrange[800],
-            heroTag: null,
-            child: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => ListTitlePage()));
-            },
-          ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        );
-      },
+                            );
+                          },
+                        ),
+                      ],
+                    ))),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrange[800],
+        heroTag: null,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ListTitlePage()));
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
