84a437a1e64868d9ed104bc3a06905c162177840
Egemen <egemenongel@hotmail.com>
2021-11-19 11:03:14 +0300

    Remove the list at the bottom

--- lib/pages/add_list_page.dart
+++ lib/pages/add_list_page.dart
@@ -77,40 +77,47 @@ class AddListPage extends StatelessWidget {
               ),
               height: 300,
               child: Consumer<TaskListManager>(
-                  builder: (context, taskListManager, child) =>
-                      ListView.separated(
-                        itemCount: taskListManager.listLength,
-                        itemBuilder: (BuildContext context, int index) {
-                          var task = taskListManager.taskList[index];
-                          return ListTile(
-                            tileColor: Colors.blueGrey[100],
-                            trailing: IconButton(
-                                icon: const Icon(
-                                  Icons.remove_circle,
-                                ),
-                                onPressed: () =>
-                                    taskListManager.removeTask(task)),
-                            title: Row(
-                              children: [
-                                Text(
-                                  "${index + 1}. ",
-                                  style:
-                                      const TextStyle(color: Colors.blueAccent),
-                                ),
-                                Text(
-                                  task.title,
-                                ),
-                              ],
+                  builder: (context, taskListManager, child) {
+                if (taskListManager.taskList.isNotEmpty) {
+                  return ListView.separated(
+                    itemCount: taskListManager.listLength,
+                    itemBuilder: (BuildContext context, int index) {
+                      var task = taskListManager.taskList[index];
+                      return ListTile(
+                        tileColor: Colors.blueGrey[100],
+                        trailing: IconButton(
+                            icon: const Icon(
+                              Icons.remove_circle,
                             ),
-                          );
-                        },
-                        separatorBuilder: (BuildContext context, int index) {
-                          return const Divider(
-                            height: 0,
-                            thickness: 2.0,
-                          );
-                        },
-                      )),
+                            onPressed: () => taskListManager.removeTask(task)),
+                        title: Row(
+                          children: [
+                            Text(
+                              "${index + 1}. ",
+                              style: const TextStyle(color: Colors.blueAccent),
+                            ),
+                            Text(
+                              task.title,
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return const Divider(
+                        height: 0,
+                        thickness: 2.0,
+                      );
+                    },
+                  );
+                } else {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                    ),
+                  );
+                }
+              }),
             ),
           ],
         ),
