0a8f784d817fe0554ea2c899437032646851d8b9
Egemen <egemenongel@hotmail.com>
2021-11-21 19:57:36 +0300

    Add task form border

--- lib/shared/form_constants.dart
+++ lib/shared/form_constants.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-OutlineInputBorder formFieldBorder() {
+OutlineInputBorder authFormBorder() {
   return const OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(20)));
+}
+
+OutlineInputBorder taskFormBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: const BorderSide(color: Colors.blueGrey),
+  );
 }
