095e74ccaf92d65d7d35788726cff40d9dd4c780
Egemen <egemenongel@hotmail.com>
2021-11-22 13:34:32 +0300

    Add deadline field

--- lib/pages/add_list/list_title_page.dart
+++ lib/pages/add_list/list_title_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:to_deer/models/list.dart';
 import 'package:to_deer/pages/add_list/add_list_page.dart';
+import 'package:to_deer/shared/form_constants.dart';
+import 'package:to_deer/shared/task_form/date_field.dart';
 import 'package:to_deer/utils/task_list_manager.dart';
 
 class ListTitlePage extends StatelessWidget {
   ListTitlePage({Key? key}) : super(key: key);
   final listTitle = TextEditingController();
+  final deadline = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -20,20 +24,39 @@ class ListTitlePage extends StatelessWidget {
           children: [
             Form(
               key: _formKey,
-              child: TextFormField(
-                controller: listTitle,
-                decoration: const InputDecoration(
-                    labelText: "List Title",
-                    labelStyle: TextStyle(fontSize: 14)),
-                autofocus: true,
-                validator: (val) =>
-                    listTitle.text.isEmpty ? "Please enter a list title" : null,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: listTitle,
+                    decoration: InputDecoration(
+                        labelText: "List Title",
+                        labelStyle: const TextStyle(fontSize: 14),
+                        enabledBorder:
+                            listTitle.text.isNotEmpty ? taskFormBorder() : null,
+                        border: taskFormBorder()),
+                    autofocus: true,
+                    validator: (val) => listTitle.text.isEmpty
+                        ? "Please enter a list title"
+                        : null,
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+                  DateField(
+                    labelText: "Deadline",
+                    controller: deadline,
+                  ),
+                ],
               ),
             ),
             ElevatedButton(
               onPressed: () {
                 if (_formKey.currentState!.validate()) {
-                  context.read<TaskListManager>().setTitle(listTitle.text);
+                  ListModel list = ListModel(
+                    title: listTitle.text,
+                    dueDate: deadline.text,
+                  );
+                  context.read<TaskListManager>().newList(list);
                   Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(
