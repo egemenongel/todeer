8bab54bbaa2f2e1c7088dc7f02ffd81760a2135c
Egemen <egemenongel@hotmail.com>
2021-11-28 16:42:23 +0300

    Fix the  incorrect use of parent widget

--- lib/pages/list/list_page.dart
+++ lib/pages/list/list_page.dart
@@ -42,117 +42,115 @@ class ListPage extends StatelessWidget {
           ),
           body: Container(
             color: Colors.red,
-            child: Expanded(
-                child: Container(
-                    width: displayWidth(context),
-                    padding: const EdgeInsets.only(top: 20),
-                    decoration: const BoxDecoration(
-                      color: Colors.indigo,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10)),
+            child: Container(
+                width: displayWidth(context),
+                padding: const EdgeInsets.only(top: 20),
+                decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)),
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: ListView.separated(
+                        itemCount: snapshot.data.docs.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          var task = snapshot.data.docs[index];
+                          return TaskTile(
+                              index: index,
+                              task: TaskModel(
+                                title: task["title"],
+                                startTime: task["startTime"],
+                                finishTime: task["finishTime"],
+                                duration: task["duration"],
+                                isCompleted: task["isCompleted"],
+                                dueDate: task["dueDate"],
+                                notes: task["notes"],
+                              ),
+                              sortedList:
+                                  firestore.orderedTasks(list!.reference),
+                              checkboxCallback: (checkboxState) => firestore
+                                  .checkboxToggle(task, checkboxState!),
+                              deleteCallback: () => showDialog(
+                                    context: context,
+                                    builder: (context) =>
+                                        DeleteTaskDialog(task: task),
+                                  ));
+                        },
+                        padding: const EdgeInsets.only(
+                          left: 10.0,
+                          right: 5.0,
+                        ),
+                        separatorBuilder: (BuildContext context, int index) {
+                          return const Divider(
+                            height: 10,
+                            color: Colors.transparent,
+                          );
+                        },
+                      ),
                     ),
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: ListView.separated(
-                            itemCount: snapshot.data.docs.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              var task = snapshot.data.docs[index];
-                              return TaskTile(
-                                  index: index,
-                                  task: TaskModel(
-                                    title: task["title"],
-                                    startTime: task["startTime"],
-                                    finishTime: task["finishTime"],
-                                    duration: task["duration"],
-                                    isCompleted: task["isCompleted"],
-                                    dueDate: task["dueDate"],
-                                    notes: task["notes"],
-                                  ),
-                                  sortedList:
-                                      firestore.orderedTasks(list!.reference),
-                                  checkboxCallback: (checkboxState) => firestore
-                                      .checkboxToggle(task, checkboxState!),
-                                  deleteCallback: () => showDialog(
-                                        context: context,
-                                        builder: (context) =>
-                                            DeleteTaskDialog(task: task),
-                                      ));
-                            },
-                            padding: const EdgeInsets.only(
-                              left: 10.0,
-                              right: 5.0,
+                    Container(
+                      height: displayHeight(context) < displayWidth(context)
+                          ? displayWidth(context) / 16
+                          : bottomBar(context),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10)),
+                        color: Colors.indigoAccent,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          FloatingActionButton(
+                            tooltip: "Add task",
+                            heroTag: null,
+                            onPressed: () => showDialog(
+                              context: context,
+                              builder: (_) => AddTaskDialog(list: list!),
                             ),
-                            separatorBuilder:
-                                (BuildContext context, int index) {
-                              return const Divider(
-                                height: 10,
-                                color: Colors.transparent,
+                            child: const Icon(
+                              Icons.add,
+                            ),
+                          ),
+                          FloatingActionButton(
+                            tooltip: "See report",
+                            heroTag: null,
+                            onPressed: () {
+                              Provider.of<TaskListManager>(context,
+                                      listen: false)
+                                  .sumDuration(snapshot.data.docs);
+                              showModalBottomSheet(
+                                backgroundColor: Colors.transparent,
+                                context: context,
+                                builder: (context) => ReportWidget(
+                                    list: list, title: list!.get("title")),
                               );
                             },
+                            child: const Icon(
+                              Icons.pending_actions_rounded,
+                            ),
                           ),
-                        ),
-                        Container(
-                          height: displayHeight(context) < displayWidth(context)
-                              ? displayWidth(context) / 16
-                              : bottomBar(context),
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10),
-                                topRight: Radius.circular(10)),
-                            color: Colors.indigoAccent,
+                          FloatingActionButton(
+                            tooltip: "Add list",
+                            heroTag: null,
+                            child: const Icon(
+                              Icons.my_library_add_outlined,
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => AddListPage()));
+                            },
                           ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              FloatingActionButton(
-                                tooltip: "Add task",
-                                heroTag: null,
-                                onPressed: () => showDialog(
-                                  context: context,
-                                  builder: (_) => AddTaskDialog(list: list!),
-                                ),
-                                child: const Icon(
-                                  Icons.add,
-                                ),
-                              ),
-                              FloatingActionButton(
-                                tooltip: "See report",
-                                heroTag: null,
-                                onPressed: () {
-                                  Provider.of<TaskListManager>(context,
-                                          listen: false)
-                                      .sumDuration(snapshot.data.docs);
-                                  showModalBottomSheet(
-                                    backgroundColor: Colors.transparent,
-                                    context: context,
-                                    builder: (context) => ReportWidget(
-                                        list: list, title: list!.get("title")),
-                                  );
-                                },
-                                child: const Icon(
-                                  Icons.pending_actions_rounded,
-                                ),
-                              ),
-                              FloatingActionButton(
-                                tooltip: "Add list",
-                                heroTag: null,
-                                child: const Icon(
-                                  Icons.my_library_add_outlined,
-                                ),
-                                onPressed: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => AddListPage()));
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ))),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
           ),
         );
       },
