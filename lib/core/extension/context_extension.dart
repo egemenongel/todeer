c4730144a648d23624407ec76446939bc44ce5d2
Egemen Öngel <egemenongel@hotmail.com>
2022-01-09 20:33:09 +0300

    Implement context extension

---
+++ lib/core/extension/context_extension.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+extension ContextExtension on BuildContext {
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+}
+
+extension MediaQueryExtension on BuildContext {
+  double get height => mediaQuery.size.height;
+  double get width => mediaQuery.size.width;
+
+  double get screenHeight =>
+      mediaQuery.size.height - kToolbarHeight - kToolbarHeight * 1.2;
+
+  double dynamicHeight(double value) => height * value;
+  double dynamicWidth(double value) => width * value;
+
+  double get lowValue => height * 0.01;
+  double get normalValue => height * 0.02;
+  double get mediumValue => height * 0.04;
+  double get highValue => height * 0.1;
+}
+
+extension PaddingExtensionAll on BuildContext {
+  EdgeInsets get paddingLow => EdgeInsets.all(lowValue);
+  EdgeInsets get paddingNormal => EdgeInsets.all(normalValue);
+  EdgeInsets get paddingMedium => EdgeInsets.all(mediumValue);
+  EdgeInsets get paddingHigh => EdgeInsets.all(highValue);
+}
+
+extension PaddingExtensionSymmeric on BuildContext {
+  EdgeInsets get paddingLowHorizontal =>
+      EdgeInsets.symmetric(horizontal: lowValue);
+  EdgeInsets get paddingNormalHorizontal =>
+      EdgeInsets.symmetric(horizontal: normalValue);
+  EdgeInsets get paddingMediumHorizontal =>
+      EdgeInsets.symmetric(horizontal: mediumValue);
+  EdgeInsets get paddingHighHorizontal =>
+      EdgeInsets.symmetric(horizontal: highValue);
+
+  EdgeInsets get paddingLowVertical => EdgeInsets.symmetric(vertical: lowValue);
+  EdgeInsets get paddingNormalVertical =>
+      EdgeInsets.symmetric(vertical: normalValue);
+  EdgeInsets get paddingMediumVertical =>
+      EdgeInsets.symmetric(vertical: mediumValue);
+  EdgeInsets get paddingHighVertical =>
+      EdgeInsets.symmetric(vertical: highValue);
+}
+
+extension RadiusExtension on BuildContext {
+  Radius get lowRadius => Radius.circular(width * 0.02);
+  Radius get normalRadius => Radius.circular(width * 0.05);
+  Radius get highRadius => Radius.circular(width * 0.1);
+}
+
+extension ThemeExtension on BuildContext {
+  ThemeData get theme => Theme.of(this);
+  TextTheme get textTheme => theme.textTheme;
+  ColorScheme get colors => theme.colorScheme;
+}
