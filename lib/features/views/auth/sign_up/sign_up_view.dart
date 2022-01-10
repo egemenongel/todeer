3856272aab086e32e3adeb7a120eed1918608e4d
Egemen Öngel <egemenongel@hotmail.com>
2022-01-10 11:56:53 +0300

    Update Sign Up View

--- lib/features/views/auth/sign_up/sign_up_view.dart
+++ lib/features/views/auth/sign_up/sign_up_view.dart
@@ -5,15 +5,22 @@ import 'package:to_deer/core/extension/context_extension.dart';
 import 'package:to_deer/features/services/auth_service.dart';
 import 'package:to_deer/features/utils/form_manager.dart';
 
-class SignUpPage extends StatelessWidget {
-  SignUpPage({
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({
     Key? key,
     this.toggleCallback,
   }) : super(key: key);
+  final VoidCallback? toggleCallback;
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final VoidCallback? toggleCallback;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,105 +28,176 @@ class SignUpPage extends StatelessWidget {
       child: Container(
         height: context.height,
         decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color(0xff050952),
-            Color(0xff09119E),
-          ]),
+          gradient: LinearGradient(
+            colors: [
+              Color(0xffAC2216),
+              Color(0xffCA6357),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
         ),
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           children: [
-            Image.asset(
-              "images/icons/24.png",
-              color: Colors.white,
+            Expanded(
+              flex: 11,
+              child: buildLogo(),
             ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: emailController,
-                    validator: (val) =>
-                        val!.isEmpty ? "Please enter your email" : null,
-                    decoration: InputDecoration(
-                        filled: true,
-                        prefixIcon: const Icon(Icons.account_circle_rounded),
-                        labelText: "E-mail",
-                        border: FormConstants.authFormBorder()),
-                  ),
-                  const SizedBox(height: 15.0),
-                  TextFormField(
-                    controller: passwordController,
-                    validator: (val) =>
-                        val!.isEmpty ? "Please enter your password" : null,
-                    decoration: InputDecoration(
-                        filled: true,
-                        prefixIcon: const Icon(Icons.vpn_key_rounded),
-                        labelText: "Password",
-                        border: FormConstants.authFormBorder()),
-                  ),
-                ],
-              ),
+            const Spacer(
+              flex: 2,
             ),
-            const SizedBox(
-              height: 10.0,
+            Expanded(
+              flex: 7,
+              child: buildForm(),
             ),
-            ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    var result = await context.read<AuthService>().signUp(
-                        email: emailController.text,
-                        password: passwordController.text);
-                    context.read<FormManager>().setSigUpError(result);
-                  }
-                },
-                child: const Text("Sign Up")),
-            const SizedBox(
-              height: 5.0,
+            Expanded(
+              flex: 4,
+              child: buildSignUpButton(context),
             ),
-            Consumer<FormManager>(builder: (_, formManager, __) {
-              return Text(
-                formManager.singUpErrorText,
-                style: const TextStyle(
-                  color: Colors.red,
-                ),
-              );
-            }),
-            const SizedBox(
-              height: 10.0,
+            Expanded(
+              flex: 2,
+              child: buildErrorText(),
             ),
-            Row(
-              children: [
-                const Expanded(
-                  child: Divider(
-                    color: Colors.grey,
-                    thickness: 1,
-                    indent: 15,
-                    endIndent: 5,
-                  ),
-                ),
-                Text(
-                  "OR",
-                  style: TextStyle(color: Theme.of(context).primaryColor),
-                ),
-                const Expanded(
-                  child: Divider(
-                    color: Colors.grey,
-                    thickness: 1,
-                    indent: 5,
-                    endIndent: 15,
-                  ),
-                ),
-              ],
+            Expanded(
+              child: buildDivider(context),
             ),
-            TextButton(
-              onPressed: toggleCallback,
-              child: const Text("LOGIN"),
-            )
+            Expanded(
+              flex: 2,
+              child: buildLoginButton(context),
+            ),
+            const Spacer(
+              flex: 4,
+            ),
             //Snackbar -> succesfully created
           ],
         ),
       ),
     ));
+  }
+
+  Image buildLogo() {
+    return Image.asset(
+      "images/icons/24.png",
+      color: Colors.white,
+    );
+  }
+
+  Form buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: emailController,
+            validator: (val) => val!.isEmpty ? "Please enter your email" : null,
+            decoration: InputDecoration(
+              filled: true,
+              prefixIcon: const Icon(Icons.account_circle_rounded),
+              labelText: "E-mail",
+              border: FormConstants.authFormBorder(),
+              enabledBorder: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+                borderSide: BorderSide(
+                  color: Color(0xffB9584F),
+                ),
+              ),
+              focusedBorder: FormConstants.authFormBorder(),
+            ),
+          ),
+          const Spacer(),
+          TextFormField(
+            controller: passwordController,
+            validator: (val) =>
+                val!.isEmpty ? "Please enter your password" : null,
+            decoration: InputDecoration(
+              filled: true,
+              prefixIcon: const Icon(Icons.vpn_key_rounded),
+              labelText: "Password",
+              border: FormConstants.authFormBorder(),
+              enabledBorder: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+                borderSide: BorderSide(
+                  color: Color(0xffB9584F),
+                ),
+              ),
+              focusedBorder: FormConstants.authFormBorder(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Row buildSignUpButton(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+            onPressed: () async {
+              if (_formKey.currentState!.validate()) {
+                var result = await context.read<AuthService>().signUp(
+                    email: emailController.text,
+                    password: passwordController.text);
+                context.read<FormManager>().setSigUpError(result);
+              }
+            },
+            child: const Text("Sign Up")),
+      ],
+    );
+  }
+
+  Consumer<FormManager> buildErrorText() {
+    return Consumer<FormManager>(builder: (_, formManager, __) {
+      return Text(
+        formManager.singUpErrorText,
+        style: const TextStyle(
+          color: Colors.red,
+        ),
+      );
+    });
+  }
+
+  Row buildDivider(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Divider(
+            color: context.colors.secondary,
+            thickness: 1,
+            indent: 15,
+            endIndent: 5,
+          ),
+        ),
+        Text(
+          "OR",
+          style: TextStyle(
+            color: context.colors.onPrimary,
+          ),
+        ),
+        Expanded(
+          child: Divider(
+            color: context.colors.secondary,
+            thickness: 1,
+            indent: 5,
+            endIndent: 15,
+          ),
+        ),
+      ],
+    );
+  }
+
+  TextButton buildLoginButton(BuildContext context) {
+    return TextButton(
+      onPressed: widget.toggleCallback,
+      child: Text(
+        "Login",
+        style: TextStyle(color: context.colors.secondary),
+      ),
+    );
   }
 }
